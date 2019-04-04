1f06e36c3a1870198f21a1d253efb6f087d1c32e
Misha Vakulich <m.vakulich@softteco.com>
2019-04-04 22:57:07 +0300

    fix android build

--- bin/lib/android.js
+++ bin/lib/android.js
@@ -154,12 +154,12 @@ module.exports = function (context) {
 
 		return fs.exists('platforms/android')
 			// Write preferences xml file
-			.then(function () { return fs.mkdir('platforms/android/res/xml'); })
-			.then(function () { return fs.writeFile('platforms/android/res/xml/apppreferences.xml', preferencesDocument.write()); })
+			.then(function () { return fs.mkdir('platforms/android/app/src/main/res/xml/'); })
+			.then(function () { return fs.writeFile('/platforms/android/app/src/main/res/xml/apppreferences.xml', preferencesDocument.write()); })
 
 			// Write localization resource file
 			.then(function () { return fs.mkdir('platforms/android/res/values'); })
-			.then(function (prefs) { return fs.writeFile('platforms/android/res/values/apppreferences.xml', preferencesStringDocument.write()); })
+			.then(function (prefs) { return fs.writeFile('/platforms/android/app/src/main/res/xml/apppreferences.xml', preferencesStringDocument.write()); })
 
 			.then(function () { console.log('android preferences file was successfully generated'); })
 			.catch(function (err) {
