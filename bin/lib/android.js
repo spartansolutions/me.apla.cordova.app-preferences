9fea006d1a8f0f1c0f0867bb6900d3604b914c88
Misha Vakulich <m.vakulich@softteco.com>
2019-04-09 22:25:38 +0300

    fix plugin installation

--- bin/lib/android.js
+++ bin/lib/android.js
@@ -191,7 +191,8 @@ module.exports = function (context) {
 			})
 			.then(function (data) {
 				var androidPackagePath = "me.apla.cordova".replace (/\./g, '/');
-				var activityFileName= path.join ('platforms/android/src', androidPackagePath, 'AppPreferencesActivity.java');
+
+				var activityFileName= path.join ('platforms/android/app/src/main/java', androidPackagePath, 'AppPreferencesActivity.java');
 
 				return fs.writeFile(activityFileName, data);
 			})
